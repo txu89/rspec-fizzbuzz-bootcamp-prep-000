87775956d48a518552c9ce4a146989cf01a5372b
Tom Xu <tomxu89@gmail.com>
2018-02-01 04:38:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
   if (int % 3 == 0 && int % 5 == 0)
     return "FizzBuzz"
   end
-  elseif int % 3 == 0
+  else if int % 3 == 0
     return "Fizz"
   end
-  elseif int % 5 == 0
+  else if int % 5 == 0
     return "Buzz"
   end
   else 
