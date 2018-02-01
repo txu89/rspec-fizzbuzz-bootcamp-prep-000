b7ba3ad1d5eb9eb5c5328ae7f6035f3c09cd8c16
Tom Xu <tomxu89@gmail.com>
2018-02-01 04:40:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if (int % 3 == 0 && int % 5 == 0)
     return "FizzBuzz"
-  elseif (int % 3 == 0)
+  elsif (int % 3 == 0)
     return "Fizz"
-  elseif (int % 5 == 0)
+  elsif (int % 5 == 0)
     return "Buzz"
   else 
     return nil
